1a356094d6188a0a8025851dab3736d80759f426
Nahom <kiflunahom1994@gmail.com>
2023-07-13 06:45:12 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - function that output
+ * Return: must have retur value 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - function that output
+ * must have return : always zer
+ */
+
+int main(void)
+{
+ 
+   puts("\"Programming is like building a multilingual puzzle");
+   
+   return (0);		   
+}   
