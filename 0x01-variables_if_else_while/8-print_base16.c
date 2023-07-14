8b4ce21db071c22a97672b25afa777f905b00a1e
Nahom <kiflunahom1994@gmail.com>
2023-07-14 06:57:12 -0700

    hxadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char letter;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
