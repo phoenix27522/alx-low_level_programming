bac6080070cae89bcb05a0bc1b6d00e23431b6c8
Nahom <kiflunahom1994@gmail.com>
2023-07-14 04:56:38 -0700

    upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - out puts the capital and small letter in a line
+ * Return: is zero
+ */
+
+int main(void)
+{
+	char upper, lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		putchar (lower);
+	}
+
+	for (upper = 'A'; upper <= 'z'; upper++)
+	{
+		putchar (upper);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
