78a2f5b8970329de8ee236ad0ef547d1e0d9ae43
Nahom <kiflunahom1994@gmail.com>
2023-07-14 07:48:54 -0700

    combination of two digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - out put the the combination of two digit
+ * Return: is zero
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			if (num1 != num2)
+			{
+				putchar('0' + num1);
+				putchar('0' + num2);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
