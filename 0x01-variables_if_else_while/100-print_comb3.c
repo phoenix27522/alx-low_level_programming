be3bda1ac8e0861a2cf3717e9094e0de9c016525
Nahom <kiflunahom1994@gmail.com>
2023-07-14 08:15:29 -0700

    combination of two digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,10 @@ int main(void)
 	{
 		for (num2 = 0; num2 <= 9; num2++)
 		{
-			putchar('0' + num1);
-			putchar('0' + num2);
-
 			if (num1 != num2 && num1 < num2)
 			{
+				putchar('0' + num1);
+				putchar('0' + num2);
 				putchar(',');
 				putchar(' ');
 			}
