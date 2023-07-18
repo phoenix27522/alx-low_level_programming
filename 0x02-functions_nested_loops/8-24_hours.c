51f9b32e2e221e359c4acce75355d293d020326c
Nahom <kiflunahom1994@gmail.com>
2023-07-18 08:22:12 -0700

    ticktock

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every miniut of the 24 hour of Jack Bauer
+ * Return: from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar('0' + hr / 10);
+			_putchar('0' + hr % 10);
+			_putchar(':');
+			_putchar('0' + min / 10);
+			_putchar('0' + min % 10);
+			_putchar('\n');
+		}
+	}
+}
