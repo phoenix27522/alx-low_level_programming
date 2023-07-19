5b8589c2c074a1028537bca818baa473d4afabaa
Nahom <kiflunahom1994@gmail.com>
2023-07-18 17:35:58 -0700

    the first 98 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * print_fibonacci - ittrates the next fibonacci number
+ */
+void print_fibonacci(void)
+{
+	unsigned long oldsum = 1, currentSum = 2, newsum;
+	int count = 2;
+
+	printf("%lu, %lu, ", oldsum, currentSum);
+
+	while (count < 98)
+	{
+		newsum = currentSum + oldsum;
+		printf("%lu", newsum);
+
+		count++;
+
+		if (count < 98)
+			printf(", ");
+		else
+			printf("\n");
+
+		oldsum = currentSum;
+		currentSum = newsum;
+	}
+}
+/**
+ * main - prints the first 50 fibonacchi numbers
+ * Return: is zero
+ */
+int main(void)
+{
+
+	print_fibonacci();
+
+	return (0);
+}
