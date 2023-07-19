71c433a17e7e997b8ed6e80219625a0401365c23
Nahom kiflu <kiflunahom1994@gmail.com>
2023-07-19 14:56:57 +0300

    my first debuging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - it out puts negative posetive and zero based on the condition
+ * Return: is zero
+ */
+void positive_or_negative(int i)
+{
+
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
