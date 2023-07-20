0f2dea75ab0ead99c65abf5bc641a4e56819099d
Nahom kiflu <kiflunahom1994@gmail.com>
2023-07-21 01:21:57 +0300

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,13 +7,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i == 0)
-		{
-			printf("%d ", i);
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("fizzbuzz ");
 		}
