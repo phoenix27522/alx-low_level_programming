b901ddb07755176322d5ed1d3e9a5ed96e197d43
Nahom kiflu <kiflunahom1994@gmail.com>
2023-07-21 05:24:54 +0300

    integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+ #include "main.h"
+/**
+ * print_number - prints an int
+ * @n: int to be assigned
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		num = -n;
+		_putchar('-');
+	}
+	else
+	{
+		num = n;
+	}
+
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+
+	_putchar((num % 10) + '0');
+}
