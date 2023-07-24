be40122346f9d17385a752e51d6066746a77a2ee
Nahom kiflu <kiflunahom1994@gmail.com>
2023-07-24 14:57:44 +0300

    every other

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,26 @@
 #include "main.h"
 /**
- * puts2 - Prints every other character of a string
- * @str: Input string
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int length = 0;
+	int i;
 
-	while (str[i])
+	while (str[length] != '\0')
 	{
-		_putchar(str[i]);
-
-		i = i + 2;
+		length++;
 	}
 
+	for (i = 0 ; i <= length - 1 ; i++)
+	{
+		if (i % 2 == 0)
+		{
+		_putchar(str[i]);
+		}
+	}
 	_putchar('\n');
 }
