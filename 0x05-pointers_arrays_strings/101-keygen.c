574daa48e2e33c36641d4e1929083ecada28ae47
Nahom kiflu <kiflunahom1994@gmail.com>
2023-07-24 19:38:08 +0300

    random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,65 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 
 /**
- * rand_Char - it helps to randomaize
- * @charSet: to be randomize
- * Return: charset
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
  */
-char rand_Char(char *charSet)
-{
-	int index;
-
-	index = rand() % strlen(charSet);
-	return (charSet[index]);
-}
-
-/**
- * generatePassword - generate a random password of the given length
- * @length: length of password
- */
-void generatePassword(int length)
-{
-	char lower_case[] = "abcdefghijklmnopqrstuvwxyz";
-	char upper_case[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char digits[] = "123456789";
-	char special[] = "!@#$%^&*()_+-=[]{}|;:,.<>?";
-	char password[length + 1];
-	int i;
-
-	for (i = 0; i < length; i++)
-	{
-		if (i % 4 == 0)
-			password[i] = rand_Char(lower_case);
-		else if (i % 4 == 1)
-			password[i] = rand_Char(upper_case);
-		else if (i % 4 == 2)
-			password[i] = rand_Char(digits);
-		else
-			password[i] = rand_Char(special);
-	}
-	password[length] = '\0';
-
-	printf("Generated Password: %s\n", password);
-
-}
-/**
- * main - first seeds z random number generator using the srand functio
- * with the current time (time(0)) as the seed.
- * This step is crucial to ensure that each run of the program
- * genrates random numbers
- * Return: is zero
- */
-
 int main(void)
 {
-	srand(time(0));
+	int pass[100];
+	int i, sum, n;
 
-	int passwordLength = 10;
+	sum = 0;	
 
-	generatePassword(passwordLength);
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 
 	return (0);
 }
