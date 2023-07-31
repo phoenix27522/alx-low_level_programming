6cc32345ff77e1f53b1e5077a1bd93a293ae9ab1
Nahom kiflu <kiflunahom1994@gmail.com>
2023-07-31 21:15:03 +0300

    locates a character in a string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strspn -  a function that gets the length of a prefix substring
+ * @s: input
+ * @accept: input
+ * Return: the length
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int len = 0;
+	int i;
+	int j;
+	int found;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		found = 0;
+		for (j = 0; j != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+				found = 1;
+				break;
+			}
+		}
+		if (!found)
+		{
+			break;
+		}
+	}
+	return (len);
+}
+
