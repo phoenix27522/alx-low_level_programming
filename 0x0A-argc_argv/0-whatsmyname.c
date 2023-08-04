f0591342953cff9f64f072843bb54d94995c177a
Nahom kiflu <kiflunahom1994@gmail.com>
2023-08-04 17:53:20 +0300

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - return int
+ * @args: length of the argument
+ * @argv: char array of the argument u enter
+ * Return: return int
+ */
+
+int main(int args, char *argv[])
+{
+	(void)args;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+==========> a program that prints its name, followed by a new line <===========
+
