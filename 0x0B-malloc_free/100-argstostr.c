e88a4a63743b031b719c2c33145724a14a971ebd
Nahom kiflu <kiflunahom1994@gmail.com>
2023-08-09 00:23:51 +0300

    concatenates all the arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - prints args
+ * @ac: takes in width of grid
+ * @av: height of grid
+ * Return: the args one line at a time
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *arg;
+	int count = 0, i = 0, j = 0, c = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	while (i < ac)
+	{
+		j = 0;
+
+		while (av[i][j] != '\0')
+		{
+			count++;
+			j++;
+		}
+		i++;
+	}
+
+	count = count + ac + 1;
+
+	arg = (char *)malloc(sizeof(char) * count);
+
+	if (arg == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			arg[c] = av[i][j];
+			c++;
+		}
+
+		arg[c] = '\n';
+		c++;
+	}
+
+	return (arg);
+}
