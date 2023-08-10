ced761456cedc3bfca40bcf2d83f3f8f09d48c01
Nahom kiflu <kiflunahom1994@gmail.com>
2023-08-10 22:59:55 +0300

    relloc (try-10)

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
@@ -28,12 +28,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (!new_ptr)
 		return (NULL);
-
-	if (new_size == 0 && ptr)
-	{
-		free(ptr);
-		return (NULL);
-	}
 
 	if (new_size > old_size)
 		new_size = old_size;
