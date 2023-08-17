7296d87bd22a5806e70e64170d4aae78ad639fe9
Nahom kiflu <kiflunahom1994@gmail.com>
2023-08-17 21:15:38 +0300

    print num

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,11 +10,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int num;
 	va_list args;
 
-	/*if (separator == NULL)
-	{
-		return;
-	}*/
-
 	va_start(args, n);
 
 	for (i = 0; i < n; ++i)
