90774e47686737321839e51a66ddeb5252ca6fbc
Nahom kiflu <kiflunahom1994@gmail.com>
2023-08-17 22:06:50 +0300

    printing string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,7 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (!string)
+		if (string == NULL)
 			string = "(nil)";
 
 		string = va_arg(args, char*);
