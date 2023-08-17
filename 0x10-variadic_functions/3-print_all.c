b304bd14f7ce016f1dd1a2dfbd7fbd500b286c7d
Nahom kiflu <kiflunahom1994@gmail.com>
2023-08-17 23:00:52 +0300

    in to printf we go

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,41 +1,49 @@
-#include <stdarg.h>
-#include <stdio.h>
-
-void print_all(const char * const format, ...) {
+#include "variadic_functions.h"
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
 	va_list args;
 	char c_val;
 	int i_val;
 	float f_val;
-	char *s_val,*sep = "";
+	char *s_val, *sep = "";
 	unsigned int i;
 
 	va_start(args, format);
 
 	i = 0;
-	while (format[i]) {
-		if (format[i] == 'c') {
+
+	while (format[i])
+	{
+		if (format[i] == 'c')
+		{
 			c_val = va_arg(args, int);
 			printf("%s%c", sep, c_val);
-		} else if (format[i] == 'i') {
+		}
+		else if (format[i] == 'i')
+		{
 			i_val = va_arg(args, int);
 			printf("%s%d", sep, i_val);
-		} else if (format[i] == 'f') {
+		}
+		else if (format[i] == 'f')
+		{
 			f_val = va_arg(args, double);
 			printf("%s%f", sep, f_val);
-		} else if (format[i] == 's') {
+		}
+		else if (format[i] == 's')
+		{
 			s_val = va_arg(args, char *);
-			if (s_val != NULL) {
-				printf("%s%s", sep, s_val);
-			} else {
-				printf("(nil)");
-			}
+			if (!s_val)
+				s_val = "(nill)";
+			printf("%s%s", sep, s_val);
 		}
 		sep = ", ";
 		i++;
 	}
-
 	va_end(args);
-
 	printf("\n");
 }
 
