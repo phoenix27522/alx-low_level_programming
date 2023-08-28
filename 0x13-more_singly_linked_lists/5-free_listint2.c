c7ce595d6370878a137efb9fe894e8831b653078
Nahom kiflu <kiflunahom1994@gmail.com>
2023-08-29 01:36:24 +0300

    forgot that it was double pointer

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,17 +5,13 @@
  */
 void free_listint2(listint_t **head)
 {
-        listint_t *current;
+	listint_t *current;
 
-	if (head == NULL)
-		return;
-
-        while (head)
-        {
-                current = (*head)->next;
-                free(*head);
-                *head = current;
-        }
-
+	while (*head != NULL)
+	{
+		current = (*head)->next;
+		free(*head);
+		*head = current;
+	}
 	*head = NULL;
 }
