875ff3399a990fdcd62ba5c4dc00c7025db704b6
Nahom kiflu <kiflunahom1994@gmail.com>
2023-08-28 18:22:48 +0300

    placed the files in the wrong folder

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,11 @@ size_t print_listint(const listint_t *h)
 	size_t count = 0;
 
 	if (h == NULL)
-		printf("empty");
+		return (-1);
 
 	while (h != NULL)
 	{
-		printf("%d\n", h -> n);
+		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
