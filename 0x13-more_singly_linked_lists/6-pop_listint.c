1ae2ac3aaa3fc0a1839a8ec09e30263664f274b2
Nahom kiflu <kiflunahom1994@gmail.com>
2023-08-29 02:15:32 +0300

    delete the head return its value

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_listint - removes the first from the list
+ * @head:  pointer to the first element
+ * Return: the value of the head
+ */
+int pop_listint(listint_t **head)
+{
+	int num;
+
+	listint_t *current;
+
+	if (*head != NULL)
+		return (0);
+	num = (*head)->n;
+	current = (*head)->next;
+	free(*head);
+	*head = current;
+
+	return (num);
+}
