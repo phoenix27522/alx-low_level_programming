d2509b656e9ed26aa2d60dbbb5b34b33e74a8cfd
Nahom kiflu <kiflunahom1994@gmail.com>
2023-08-28 18:18:40 +0300

    placed the files in the wrong folder

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * listint_len - prints the number elements in a list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nods
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (-1);
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
