4749117aa010b52ab5135636b048fde74ff0bdb4
Nahom kiflu <kiflunahom1994@gmail.com>
2023-09-04 17:39:58 +0300

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fileopen = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fileopen = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fileopen == -1)
 		return (-1);
 
