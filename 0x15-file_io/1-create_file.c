8dea6e59473d4717ca2a938fd1fc9208570a0596
Nahom kiflu <kiflunahom1994@gmail.com>
2023-09-04 17:36:26 +0300

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,8 +21,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; len != '\0'; len++)
-			;
+		while (*text_content != '\0')
+		{
+			len++;
+			text_content++;
+		}
 		filewrite = write(fileopen, text_content, len);
 		if (filewrite == -1)
 		{
