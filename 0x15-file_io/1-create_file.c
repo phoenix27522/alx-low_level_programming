55b92e9d09fd7c1d1fa6c4d4a8a9b282a5da50c4
Nahom kiflu <kiflunahom1994@gmail.com>
2023-09-04 17:52:04 +0300

    fixing bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,13 +15,12 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fileopen = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
+	fileopen = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	filewrite = write(fileopen, text_content, len);
 
 	if (fileopen == -1 || filewrite == -1)
