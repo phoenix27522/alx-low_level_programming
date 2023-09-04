b92fffe8ba7504d73cf0ec76b0f95cfc35ab206d
Nahom kiflu <kiflunahom1994@gmail.com>
2023-09-04 20:36:42 +0300

    fixing

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define BUFF_SIZE 1024
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -13,7 +12,5 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-char *create_buffer(char *file);
-void close_file(int fd);
 
 #endif /* MAIN_H */
