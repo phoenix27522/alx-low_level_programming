895c02ae45f9e70be0420d42bb4e9912825f131f
Nahom kiflu <kiflunahom1994@gmail.com>
2023-10-12 12:37:41 +0300

    fix on the printing double linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 /**
  * print_dlistint - print the list
- * @dlistint_t *h: a pointer to the struct
+ * @h: a pointer to the struct
  *
- * return: all the value of the list
+ * Return: all the value of the list
  *
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
+
 	while (h)
 	{
 		printf("%d\n", h->n);
